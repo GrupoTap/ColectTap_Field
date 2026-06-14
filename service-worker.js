d9839fac940609991902a517af7b1af53feb6542
GrupoTap <sousa.mecanica@gmail.com>
2026-06-14 20:18:16 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+// service-worker.js - ColectTap PWA
+const CACHE_NAME = 'colecttap-cache-v2.16';
+const FILES_TO_CACHE = [
+  './ColectTap_v2_16.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Network-first para garantir dados atualizados; cache como fallback offline
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
